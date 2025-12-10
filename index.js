4d562d6221ab94add695c5206899a4e0fb5c4d99
Nikoyaka9dd <minikkosumikko@gmail.com>
2025-12-10 22:39:30 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 250,
+    frame: false,        
+    transparent: true,     
+    vibrancy: 'hud',       
+    visualEffectState: 'active',
+    hasShadow: false,     
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
